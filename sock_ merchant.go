1b754fd4de9240a68bae2b8a342630a6cb6fdc03
ghostcodr <37741198+ghostcodr@users.noreply.github.com>
2020-05-24 10:21:57 +0530

    create sock_ merchant.go
    Hacker Rank Sock Merchant Problem

---
+++ sock_ merchant.go
@@ -0,0 +1,108 @@
+/*
+John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+For example, there are  socks with colors . There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+Function Description
+
+Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
+
+sockMerchant has the following parameter(s):
+
+n: the number of socks in the pile
+ar: the colors of each sock
+Input Format
+
+The first line contains an integer , the number of socks represented in .
+The second line contains  space-separated integers describing the colors  of the socks in the pile.
+
+Constraints
+
+ where 
+Output Format
+
+Return the total number of matching pairs of socks that John can sell.
+
+Sample Input
+
+9
+10 20 20 10 10 30 50 10 20
+
+Sample Output
+
+3
+*/
+
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+    "sort"
+)
+
+// Complete the sockMerchant function below.
+func sockMerchant(n int32, ar []int32) int32 {
+  
+    sort.Slice(ar, func(i, j int) bool { return ar[i] < ar[j] })
+    var pairs int32
+    for i := 0; i < len(ar); i++ {
+        if i < len(ar)-1 && ar[i] == ar[i+1] {
+            pairs = pairs + 1
+            i = i + 1
+        }
+    }
+    return pairs
+
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    arTemp := strings.Split(readLine(reader), " ")
+
+    var ar []int32
+
+    for i := 0; i < int(n); i++ {
+        arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+        checkError(err)
+        arItem := int32(arItemTemp)
+        ar = append(ar, arItem)
+    }
+
+    result := sockMerchant(n, ar)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
